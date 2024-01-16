a7e7478b7fa453e6a8ee18b3b6d262591e3d0906
Aurelien FERNANDEZ <aurelien.fernandez@algosup.com>
2024-01-16 14:48:55 +0100

    Implement CreateFile

--- InventoryManagementSystem/backend.h
+++ InventoryManagementSystem/backend.h
@@ -1,3 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+typedef struct product
+{
+    char id[10];
+    char name[50];
+    int quantity;
+    int price;
+}product_t;
+
+
+int CreateFile(char *filename, product_t *products)
+{
+    // Check if the file is a .txt or not
+    char *ext = strrchr(filename, '.');
+    if (!ext || strcmp(ext, ".txt") != 0)
+    {
+        return EXIT_FAILURE;
+    }
+
+    FILE *file = fopen(filename, "w");
+
+    int length= sizeof(products)/sizeof(product_t);
+    for(int count=0;count<length;count++){
+        fprintf(file,"Product n°:%s\n",products[count].id);
+        fprintf(file,"Name :%s\n",products[count].name);
+        fprintf(file,"Price:%d\n",products[count].quantity);
+        fprintf(file,"Product n°:%d\n",products[count].price); 
+    }
+
+
+    fclose(file);
+}
